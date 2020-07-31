e237fea52f6d14367c73999022dc707d7606ae4b
Bryan Bolivar <bryan.andres.bolivar@gmail.com>
2020-07-31 03:23:52 -0500

    Added Mac support and waves animation

--- KitchenSink/ContentView.swift
+++ KitchenSink/ContentView.swift
@@ -10,70 +10,103 @@ import SwiftUI
 import AVFoundation
 
 struct ContentView: View {
-
+    @State private var phase = 0.0
+    @State private var chordsOpacity = 1.0
+    
     var body: some View {
         ZStack {
-            BorderedCircle(color: Color(red: 0.87, green: 0.73, blue: 0.52),
-                           borderColor: Color(red: 0.54, green: 0.411, blue: 0.07))
             
-            VStack{
-                HStack {
-                    CircleButton(note: .MiGrave)
-                    Spacer()
-                    CircleButton(note: .Re)
+            ForEach(0..<6) { i in
+                Wave(strength: 5,
+                     frequency: 10,
+                     phase: self.phase)
+                    .stroke(Color.gray,
+                            lineWidth: 5)
+                    .offset(x: (CGFloat(i) * 50) - 120)
+                    .opacity(chordsOpacity)
+            }
+            
+            ZStack {
+                BorderedCircle(color: Color(red: 0.87, green: 0.73, blue: 0.52),
+                               borderColor: Color(red: 0.54, green: 0.411, blue: 0.07))
+                VStack{
+                    HStack {
+                        CircleButton(note: .MiGrave)
+                        Spacer()
+                        CircleButton(note: .Re)
+                    }
+                    .padding()
+                    HStack {
+                        CircleButton(note: .Mi)
+                        Spacer()
+                        CircleButton(note: .Sol)
+                    }
+                    .padding()
                 }
                 .padding()
-                HStack {
-                    CircleButton(note: .Mi)
+                
+                VStack {
+                    CircleButton(note: .La)
                     Spacer()
-                    CircleButton(note: .Sol)
+                    CircleButton(note: .Si)
                 }
                 .padding()
+                
+                Image(systemName: "music.note")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 50)
+                    .padding()
             }
-            .padding()
-            
-            VStack {
-                CircleButton(note: .La)
-                Spacer()
-                CircleButton(note: .Si)
-            }
-            .padding()
-            
-            Image(systemName: "music.note")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 50)
-                .padding()
+            .frame(width: 400, height: 400, alignment: .trailing)
+
+   
         }
-        .frame(width: 400, height: 400, alignment: .trailing)
+        .edgesIgnoringSafeArea(.all)
+        .onAppear {
+            withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+                self.phase = .pi * 2
+            }
+        }
     }
 
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.light)
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
 
 struct CircleButton: View {
     @Environment(\.colorScheme) var colorScheme
     @State var note: Note
-    
+    @State private var animation = 0.0
+    var animationSpeed = 1.0
+
     var body: some View {
-        AnimatedButton(buttonStyle: PulsingButtonStyle.self, action: {
-            print(note.rawValue)
-     
+        
+        Button(action: {
+            playAudio(note: note)
+            animation = 0.0
+            withAnimation(Animation.easeOut(duration: animationSpeed)) {
+                animation = 1
+            }
         }, label: {
             Text(note.name)
                 .font(.largeTitle)
                 .frame(width: 60, height: 60)
-        }, animationSpeed: 1, note: note)
-      
+        })
+            .buttonStyle(PulsingButtonStyle.init(animation: animation))
+        
     }
     
-
+    func playAudio(note: Note) {
+        GSAudio.sharedInstance.playSound(soundFileName: note.rawValue)
+    }
 }
 
 struct BorderedCircle: View {
@@ -131,7 +164,6 @@ struct AnimatedButton<ButtonStyle: AnimatingButtonStyle,  Content: View>: View {
             }
         }, label: label)
             .buttonStyle(buttonStyle.init(animation: animation))
-            
     }
     
     func playAudio(note: Note) {
@@ -139,3 +171,37 @@ struct AnimatedButton<ButtonStyle: AnimatingButtonStyle,  Content: View>: View {
     }
 }
 
+
+struct Wave: Shape {
+    var animatableData: Double {
+        get { phase }
+        set { self.phase = newValue }
+    }
+
+    var strength: Double
+    var frequency: Double
+    var phase: Double
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath()
+        let width = Double(rect.width)
+        let height = Double(rect.height)
+        let midWidth = width / 2
+        let midHeight = height / 2
+        let oneOverMidHeight = 1 / midHeight
+        let wavelength = height / frequency
+        path.move(to: CGPoint(x: midWidth, y: 0))
+
+        for y in stride(from: 0, through: height, by: 1) {
+            let relativeY = y / wavelength
+            let distanceFromMidHeight = y - midHeight
+            let normalDistance = oneOverMidHeight * distanceFromMidHeight
+            let parabola = -(normalDistance * normalDistance) + 1
+            let sine = sin(relativeY + phase)
+            let x = parabola * strength * sine + midWidth
+            path.addLine(to: CGPoint(x: x, y: y))
+        }
+
+        return Path(path.cgPath)
+    }
+}
